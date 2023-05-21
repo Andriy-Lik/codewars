bbfe5d2bb4770c6bdbccc824e4ea2c592e8ebcea
Andriy-Lik <alan321987@gmail.com>
2023-05-21 22:55:48 +0300

    add files

---
+++ src/components/StringToNumber.js
@@ -0,0 +1,17 @@
+// Нам нужна функция, которая может преобразовать строку в число. 
+// Какие способы достижения этого вы знаете?
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+
+function StringToNumber(str) {
+    // console.log(Number(str));
+    console.log(Number("1234"));
+    console.log(Number("605"));
+    console.log(Number("1405"));
+    console.log(Number("-7"));
+};
+
+export default StringToNumber;
